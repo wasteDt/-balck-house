c42e1df9e87c57e81b036e7ba66524fb83188aa3
wasteDt <732372889@qq.com>
2025-10-29 21:07:56 +0800

    ElementPlus的创建引用

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-<el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      
+   <div>
+      我是App
+      <test-demo></test-demo>
+      <el-button @click="$router.push('/home')">跳首页</el-button>
+      <el-button @click="goList">跳列表页</el-button>
+   </div>
 </template>
 
 
 <script  setup>
-
+   import { useRoute,useRouter } from 'vue-router';
+   const route = useRoute()
+   const router =useRouter()
+   const goList = () =>{
+        router.push('/list')
+       console.log(router,route)
+   }
 </script>
 
 <style scoped>
