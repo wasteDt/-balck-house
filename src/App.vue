7ea9e69bb25fb3bb178f94d8ee483cfab8264157
wasteDt <732372889@qq.com>
2025-10-29 19:04:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+</template>
+
+<script  setup>
+
+</script>
+
+<style scoped>
+
+</style>
